8fad5a162ee889157ba5e8b786857cc4dee54d1b
Troy Moling <troy.moling@gmail.com>
2018-02-27 14:56:30 -0600

    linking js files

--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
+var keys = require('./keys.js');
+
+var fs = require("fs");
+
+fs.readFile("keys", "utf8", function (err, data){
+  if (err) {
+    console.log(err);
+  }
+
 require("dotenv").config();
 
 var liriTalk = require("./keys.js");
@@ -9,6 +18,18 @@ var client = new Twitter(keys.twitter);
 client.get(path, params, callback);
 client.post(path, params, callback);
 client.stream(path, params, callback);
+
+var Twitter = require('twitter');
+
+//request omdb api
+request("http://www.omdbapi.com/?apikey=trilogy&s=batman", function
+(error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  var responseBodyObject = JSON.parse(body);
+  console.log(responseBodyObject.Search[0].Year);
+});
 
 //spotify request
 var Spotify = require('node-spotify-api');
@@ -26,4 +47,6 @@ spotify
   .catch(function(err) {
     console.error('Error occurred: ' + err); 
   });
+
+  
 
